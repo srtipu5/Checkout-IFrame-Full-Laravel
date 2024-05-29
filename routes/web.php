477ce36ce19d73414fe92aebfada012ca39f3b13
Sahiqur Rahman Tipu <70115206+srtipu5@users.noreply.github.com>
2024-05-29 12:32:23 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,13 @@ Route::get('/', function () {
 });
 
 // Checkout (IFrame) User Part
-Route::get('/pay', [BkashController::class, 'payment'])->name('bkash-payment');
-Route::get('/bkash/create', [BkashController::class, 'createPayment'])->name('bkash-create');
-Route::get('/bkash/execute', [BkashController::class, 'executePayment'])->name('bkash-execute');
+Route::get('/pay', [BkashController::class, 'confirm'])->name('bkash-confirm');
+Route::get('/bkash/pay', [BkashController::class, 'payment'])->name('bkash-payment');
+Route::post('/bkash/create', [BkashController::class, 'createPayment'])->name('bkash-create');
+Route::post('/bkash/execute', [BkashController::class, 'executePayment'])->name('bkash-execute');
 Route::get('/success', [BkashController::class, 'successPayment'])->name('bkash-success');
 Route::get('/fail', [BkashController::class, 'failPayment'])->name('bkash-fail');
+Route::get('/bkash/query', [BkashController::class, 'query'])->name('get-query');
 
 // Checkout (IFrame) Refund Admin Part
 Route::get('/bkash/refund', [BkashController::class, 'getRefund'])->name('get-refund');
